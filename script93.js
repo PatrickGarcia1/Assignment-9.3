d65881cb765d5b8d78c6093a465c79c029718357
PatrickGarcia1 <53844521+PatrickGarcia1@users.noreply.github.com>
2019-09-09 20:52:14 -0400

    Add files via upload

---
+++ script93.js
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4];
+
+function printReverse(numbers) {
+    for (var i = (numbers.length - 1); i >= 0; i--) {
+        {
+            console.log(numbers[i]);
+        }
+    }
+}
